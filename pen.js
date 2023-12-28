a1f7db229ba869b1163bcbf8bd9d69836320512d
zanyKhan <fatmazainab071@gmail.com>
2023-12-28 19:29:00 +0530

    Pen page using HTML

---
+++ pen.js
@@ -0,0 +1,12 @@
+const product = {
+    productName : "Ball pen",
+    color : "Black",
+    price : 270,
+    offer : 50,
+    deal : "deal of the day" 
+};
+console.log(product);
+console.log(product.price);
+console.log(product["offer"]);
+console.log(product.deal);
+ 
